9ec96e175de4f432cdd5fc78b7cb74b907b9488b
Leandro Almeida <leandro.almeida58@enova.educacao.com.br>
2022-12-16 00:26:08 -0300

    adjust in menu

--- src/components/navBars/leftNavBar/Menus/index.tsx
+++ src/components/navBars/leftNavBar/Menus/index.tsx
@@ -1,12 +1,30 @@
 import React from "react";
-import { Container } from "./styles";
-
+import { Container, ContainerMenus } from "./styles";
+import MenuTitle from "../MenuTitle";
+import MenuIcon from "../MenuIcon";
 interface IMenus {}
 
 const Menus: React.FC<IMenus> = (props) => {
   const {} = props;
+  const MenusOptions = [
+    { title: "Home", icon: null, href: "" },
+    { title: "Dashboard", icon: null, href: "" },
+    { title: "Projects", icon: null, href: "" },
+    { title: "Tasks", icon: null, href: "" },
+    { title: "Settings", icon: null, href: "" },
+    { title: "Messages", icon: null, href: "" },
+  ];
 
-  return <Container></Container>;
+  return (
+    <ContainerMenus>
+      {MenusOptions.map((menu) => (
+        <Container>
+          <MenuIcon />
+          <MenuTitle title={menu.title} />
+        </Container>
+      ))}
+    </ContainerMenus>
+  );
 };
 
 export default Menus;
--- src/components/navBars/leftNavBar/Menus/styles.tsx
+++ src/components/navBars/leftNavBar/Menus/styles.tsx
@@ -1,7 +1,16 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
+export const ContainerMenus = styled.div`
   display: flex;
+  flex-direction: column;
+  gap: 20px;
+  padding: 20px 0;
   border: 1px solid red;
   flex: 1;
+`;
+export const Container = styled.a`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  cursor: pointer;
 `;
